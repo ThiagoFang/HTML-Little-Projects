68e281b09ed86ad94e56e10d9ecfc66452a3b0d0
Thiago <thiagofang@gmail.com>
2022-10-05 12:21:07 -0300

    finalizando o js

--- Menus/Responsive-navigation-menu/script.js
+++ Menus/Responsive-navigation-menu/script.js
@@ -3,11 +3,15 @@ const closeButton = document.querySelector('.close__button');
 const searchBox = document.querySelector('.search__box');
 
 const openSearchMenu = () => {
-  searchBox.classList.add('active')
+  searchBox.classList.add('active');
+  closeButton.classList.add('active');
+  searchButton.classList.add('active');
 };
 
 const closeSearchMenu = () => {
-  searchBox.classList.remove('active')
+  searchBox.classList.remove('active');
+  closeButton.classList.remove('active');
+  searchButton.classList.remove('active');
 };
 
 searchButton.addEventListener('click', openSearchMenu);
